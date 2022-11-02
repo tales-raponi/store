e78a26d578ef6d24d1e347405b057d4a3ec86efe
Tales Raponi <raponi13@gmail.com>
2022-11-01 21:37:24 -0300

    feat: body component updated

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./index.css";
-import house1 from "../../assets/house1.jpg";
 import { BiArea } from "react-icons/bi";
 import { MdOutlineBedroomParent } from "react-icons/md";
+import Star from "../avaliation/Star";
+import { houses } from "../../domain/factories/houses";
 
 const Body = () => {
   return (
@@ -10,27 +11,40 @@ const Body = () => {
       <div className="title__container">
         <h1>More then 40 Luxury Houses</h1>
       </div>
-      <div className="house__container">
-        <div className="img__container">
-          <img src={house1} alt="" />
-        </div>
-        <div className="house-prop__container">
-          <h5>
-            <BiArea fontSize={"1rem"} />
-            320 m²
-          </h5>
-          <h5>
-            <MdOutlineBedroomParent fontSize={"1rem"} />2 main rooms
-          </h5>
-        </div>
-
-        <div className="price__container">
-          <span>$ 1.000.000,00</span>
-        </div>
-        <div className="description__container">
-          <h2></h2>
-        </div>
-      </div>
+      {houses.map((prop) => {
+        return (
+          <div className="house__container">
+            <div className="img__container">
+              <img src={prop.image} alt="" />
+              <div className="btn from-center">Show House</div>
+            </div>
+            <div className="house-prop__container">
+              <h5>
+                <BiArea fontSize={"1rem"} />
+                {prop.area} m²
+              </h5>
+              <h5>
+                <MdOutlineBedroomParent fontSize={"1rem"} />
+                {prop.rooms} main rooms
+              </h5>
+            </div>
+            <div className="price__container">
+              <span>$ {prop.price}</span>
+            </div>
+            <div className="description__container">
+              <h3>House Avaliation</h3>
+              <Star />
+              <h3>Description</h3>
+              <h5>
+                {prop.description.length > 840
+                  ? prop.description.slice(0, 840)
+                  : prop.description}
+                <a>{prop.description.length > 840 ? "...Read more" : null}</a>
+              </h5>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 };
