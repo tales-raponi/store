2b46ffae95a5f2d03e9baeace9513eb3daf3c15b
Tales Raponi <raponi13@gmail.com>
2022-11-01 21:36:44 -0300

    feat: houses factory implemented.

---
+++ src/domain/factories/houses.tsx
@@ -0,0 +1,52 @@
+import { lorem } from "./lorem";
+import house1 from "../../assets/house1.jpg";
+import house2 from "../../assets/house2.jpg";
+import house3 from "../../assets/house3.jpg";
+import house4 from "../../assets/house4.jpg";
+import house5 from "../../assets/house5.jpg";
+import house6 from "../../assets/house6.jpg";
+
+export const houses = [
+  {
+    image: house1,
+    area: "320",
+    rooms: "2",
+    price: "1.000.000,00",
+    description: lorem,
+  },
+  {
+    image: house2,
+    area: "480",
+    rooms: "4",
+    price: "10.000.000,00",
+    description: lorem,
+  },
+  {
+    image: house3,
+    area: "255",
+    rooms: "1",
+    price: "500.000,00",
+    description: lorem,
+  },
+  {
+    image: house4,
+    area: "667",
+    rooms: "6",
+    price: "23.000.000,00",
+    description: lorem,
+  },
+  {
+    image: house5,
+    area: "150",
+    rooms: "2",
+    price: "750.000,00",
+    description: lorem,
+  },
+  {
+    image: house6,
+    area: "325",
+    rooms: "3",
+    price: "7.500.000,00",
+    description: lorem,
+  },
+];
