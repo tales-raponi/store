b0ae596611ef17b5fef7e65e78d59fe5764fa4a6
Tales Raponi <raponi13@gmail.com>
2022-11-03 19:21:17 -0300

    feat: react-dom implemented / main.tsx & app.tsx changed.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import House from "./components/house/House";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="houses" element={<House />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
